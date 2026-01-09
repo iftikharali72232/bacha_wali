f6c9e5fb0d1f51d8f569ec6476ab3fe46e2ff5b7
iftikharali72232 <iftikharali72232@gmail.com>
2026-01-09 21:21:35 +0500

    ...

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
             health: '/up',
         )
         ->withMiddleware(function (Middleware $middleware): void {
-            $middleware->alias('ensure.admin', \App\Http\Middleware\EnsureUserIsAdmin::class);
+            $middleware->alias([
+                'ensure.admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+            ]);
         })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
